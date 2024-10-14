54a3fd6d4cae153407a41e2d6779e0479f072518
matt <aldraddd01@gmail.com>
2024-10-14 23:55:08 +0700

    ez-sens base

---
+++ src/Home.jsx
@@ -0,0 +1,42 @@
+
+export default function Home() {
+    return (
+        <section className="h-screen bg-neutral-950 px-[5rem] flex justify-center items-center text-white">
+            <div className="border-neutral-800 border rounded-lg h-[30rem] w-[25rem] p-[2rem] flex flex-col gap-[2rem]">
+                <div>
+                    <h1 className="text-[1.5rem]">EZ-Sens</h1>
+                    <h1 className="text-neutral-500 text-[0.8rem]">Random Sensitivity Creator</h1>
+                </div>
+                <div className="space-y-2 flex flex-col">
+                    <h1>Your Average Sensitivity:</h1>
+                    <input type="number" name="" id="avgSens" className="w-full bg-neutral-950 border-neutral-800 border rounded-lg px-[0.5rem] h-[2.5rem]"/>
+                    <h1>Deviation Difference:</h1>
+                    <input type="number" name="" id="devDiff" className="w-full bg-neutral-950 border-neutral-800 border rounded-lg px-[0.5rem] h-[2.5rem]"/>
+                    <button id="generateSens" className="flex self-center border-neutral-800 border px-[2rem] py-[0.5rem] rounded-lg transform-all duration-300 hover:bg-white hover:text-black">Randomize</button>
+                </div>
+                <div>
+                    <h1>Your New Random Sensis\tivity:</h1>
+                    <input type="text" name="" id="result" className="w-full bg-neutral-950 border-neutral-800 border rounded-lg px-[0.5rem] h-[2.5rem]"/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function getRandomSensitivity(avg, deviation) {
+    const min = avg - deviation;
+    const max = avg + deviation;
+    return Math.random() * (max - min) + min;
+}
+
+document.getElementById("generateSens").addEventListener("click", () => {
+    const avgSens = parseFloat(document.getElementById("avgSens").value);
+    const devDiff = parseFloat(document.getElementById("devDiff").value);
+
+    if (!isNaN(avgSens) && !isNaN(devDiff)) {
+        const randomSensitivity = getRandomSensitivity(avgSens, devDiff);
+        document.getElementById("result").value = randomSensitivity.toFixed(2);
+    } else {
+        document.getElementById("result").value = 'Invalid number. Try again.';
+    }
+});
